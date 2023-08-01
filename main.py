26202104af3c8b62560d72308bad23f72dc6a648
Burnnet <Burnnet00@gmail.com>
2023-08-01 12:52:47 +0300

    edit

--- main.py
+++ main.py
@@ -2,20 +2,13 @@ import requests
 
 """ The parser for site 'https://wttr.in/san%20francisco?nTqu&lang=en' """
 
-payload = {'n': '', 'T': '', 'q': '', 'm': '', 'lang': 'ru'}
-# 'q': '', silent
-# 'm': '', metric
-# 'M': '', speed_wind_m_s
+payload = {'n': '', 'T': '', 'q': '', 'M': '', 'lang': 'ru'}
 url_tmp = 'https://wttr.in/{}'
 url = url_tmp.format('SVO')
-# url_tmp = url.format('san%20francisco')
-# url = url_tmp.format('london')
-# url = url_tmp.format(''london':cherepovets:SVO')
+# url = url_tmp.format('london':cherepovets:SVO')
 
 response = requests.get(url, params=payload)
 response.raise_for_status()
 
 print(response.text)
 
-# https://wttr.in/san%20francisco?nTqu&lang=en
-# https://wttr.in/san%20francisco?n=Tqu&lang=en 2=
