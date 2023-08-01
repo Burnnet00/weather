565b580d4d0a53f06b8767a2b4e8c3130aaaeb8d
Burnnet <Burnnet00@gmail.com>
2023-08-01 21:31:36 +0300

    add readme

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ import requests
 """ The parser for site 'https://wttr.in/san%20francisco?nTqu&lang=en' """
 
 payload = {'n': '', 'T': '', 'q': '', 'M': '', 'lang': 'ru'}
-url_tmp = 'https://wttr.in/{}'
-url = url_tmp.format('SVO')
-# url = url_tmp.format('london':cherepovets:SVO')
+tmp_url = 'https://wttr.in/{}'
+url = tmp_url.format('london','cherepovets', 'SVO')
 
 response = requests.get(url, params=payload)
 response.raise_for_status()
 
 print(response.text)
-
