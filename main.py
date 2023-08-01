5adc024e84ff19871db337cdc24aa1cbfaf13883
Burnnet <Burnnet00@gmail.com>
2023-08-01 09:03:01 +0300

    add new place

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import requests
 
-""" The parser for cite 'https://wttr.in/san%20francisco?nTqu&lang=en' """
+""" The parser for site 'https://wttr.in/san%20francisco?nTqu&lang=en' """
 
-payload = {'san%20francisco?nTqu&lang': 'en'}
-url = 'https://wttr.in'
+payload = {'lang': 'en'} #'n': 'Tqu',
+url_tmp = 'https://wttr.in/{}'
+url = url_tmp.format('SVO')
+# url_tmp = url.format('san%20francisco')
+# url = url_tmp.format('london')
+# url = url_tmp.format('cherepovets')
+# url = url_tmp.format('SVO')
 response = requests.get(url, params=payload)
 response.raise_for_status()
 
 print(response.text)
+
+# https://wttr.in/san%20francisco?nTqu&lang=en
+# https://wttr.in/san%20francisco?n=Tqu&lang=en 2=
+
+
+
+
